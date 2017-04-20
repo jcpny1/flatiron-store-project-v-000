90924fc80563babe46c4ec543dfbed674d13ad03
John Pfingst <jpfingst@outlook.com>
2017-04-20 19:15:05 +0000

    cart tests passing

--- app/models/cart.rb
+++ app/models/cart.rb
@@ -2,4 +2,21 @@ class Cart < ActiveRecord::Base
   belongs_to :user
   has_many :line_items
   has_many :items, through: :line_items
+
+
+
+  def add_item(item_id)
+    line_item = self.line_items.find_or_initialize_by(item_id: item_id)
+    line_item.quantity += 1
+    line_item
+  end
+
+  def total
+    total_amount = 0.0
+    self.line_items.each { |li|
+      total_amount += li.quantity * li.item.price
+    }
+    total_amount
+  end
+
 end
--- app/models/line_item.rb
+++ app/models/line_item.rb
@@ -1,4 +1,8 @@
 class LineItem < ActiveRecord::Base
   belongs_to :cart
   belongs_to :item
+
+  after_initialize do |line_item|
+    line_item.quantity ||= 0
+  end
 end
