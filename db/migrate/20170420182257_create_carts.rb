0110804824b157ed811c6d1853a4c7c65cac6744
John Pfingst <jpfingst@outlook.com>
2017-04-21 02:51:15 +0000

    checkpoint

--- db/migrate/20170420182257_create_carts.rb
+++ db/migrate/20170420182257_create_carts.rb
@@ -2,7 +2,7 @@ class CreateCarts < ActiveRecord::Migration
   def change
     create_table :carts do |t|
       t.belongs_to :user, index: true
-      t.boolean :submitted
+      t.string :status
 
       t.timestamps null: false
     end
--- db/schema.rb
+++ db/schema.rb
@@ -15,7 +15,7 @@ ActiveRecord::Schema.define(version: 20170420182623) do
 
   create_table "carts", force: :cascade do |t|
     t.integer  "user_id"
-    t.boolean  "submitted"
+    t.string   "status"
     t.datetime "created_at", null: false
     t.datetime "updated_at", null: false
   end
