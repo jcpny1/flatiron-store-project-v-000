61133777c323cf3106f97b268ed12f53b41991b1
John Pfingst <jpfingst@outlook.com>
2017-04-21 18:04:21 +0000

    cart feature tests passing

--- app/controllers/line_items_controller.rb
+++ app/controllers/line_items_controller.rb
@@ -1,3 +1,12 @@
 class LineItemsController < ApplicationController
-
+  def create
+    if current_user.current_cart.nil?
+      new_cart = Cart.new(status: 'not submitted')
+      current_user.carts << new_cart
+      current_user.current_cart = new_cart
+      current_user.save
+    end
+    current_user.current_cart.add_item(params[:item_id])
+    redirect_to current_user.current_cart
+  end
 end
