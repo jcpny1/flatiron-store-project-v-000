0df6eaebc2213b95d0449ccc515e486185425bcf
John Pfingst <jpfingst@outlook.com>
2017-04-21 19:03:18 +0000

    store home feature tests passing

--- app/controllers/store_controller.rb
+++ app/controllers/store_controller.rb
@@ -2,6 +2,7 @@ class StoreController < ApplicationController
   def index
     @categories = Category.all
     @items = Item.all
-    @cart = current_user.current_cart
+    @user = current_user
+    @cart = current_user.current_cart if current_user
   end
 end
